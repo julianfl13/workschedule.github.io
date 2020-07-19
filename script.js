8356605f7704f89e8bad833389fcb6e4e15c1c53
Julian Lopez <julian12lopez@gmail.com>
2020-07-19 13:06:29 -0400

    script finished

--- script.js
+++ script.js
@@ -14,6 +14,29 @@ $(document).ready(function(){
             if (hourBlock < currHour) {
                 $(this).addClass("past");
               }
+            else if(hourBlock === currHour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+              }
         });
     }
-})
+            updateHour();
+    var int = setInterval(updateHours, 20000);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+});
